5f74bd6fc7547611309a05abad7a94bc478c464e
0hh <mail2dimi@gmail.com>
2025-04-04 16:46:58 +0200

    Allowance for Owner

--- contracts/SmartContractWallet.sol
+++ contracts/SmartContractWallet.sol
@@ -1,18 +1,42 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.15;
+pragma solidity 0.8.16;
 
 contract SmartContractWallet {
 
     address payable public owner;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not the contract owner");
-        _;
-    }
+    mapping(address => uint) public allowance;
+    mapping(address => bool) public isAllowedToSend;
 
     constructor() {
         owner = payable(msg.sender);  
+    }
+
+    function setAllowance(address _for, uint _amount) public {
+        require(msg.sender == owner, "You are not the owner, aborting");
+        allowance[msg.sender] = _amount;
+
+        if(_amount > 0) {
+            isAllowedToSend[msg.sender] = true;
+        } else {
+            isAllowedToSend[msg.sender] = false;
+        }
+    }
+
+    function transfer(address payable _to, uint _amount, bytes memory _payload) public returns(bytes memory){
+        //require(msg.sender == owner, "You are not the contract owner, aborting");
+        if(msg.sender != owner) {
+            require(allowance[msg.sender] >= _amount, "You are trying to send more than you are allowed to, aborting");
+            require(isAllowedToSend[msg.sender], "You are not allowed to send anything from this smart contract, aborting");
+
+            allowance[msg.sender] >= _amount;
+        }
+
+        (bool success, bytes memory returnData) = _to.call{value: _amount}(_payload);
+        require(success, "Call was not successful, aborting");
+        return returnData;
+
     }
 
     receive() external payable {}
